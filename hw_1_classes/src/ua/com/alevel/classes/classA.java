1934e3c5e2b5bb7d3dcc240f95c480c3d7b22151
Vitaminka7 <emelanova.vitaminka@gmail.com>
2024-04-12 13:56:18 +0300

    hw_1

---
+++ hw_1_classes/src/ua/com/alevel/classes/classA.java
@@ -0,0 +1,7 @@
+package ua.com.alevel.classes;
+
+public class classA {
+    public static void main (String[] args) {
+        System.out.println("Hello classA")
+    }
+}
---
+++ hw_1_classes/src/ua/com/alevel/classes/classB.java
@@ -0,0 +1,8 @@
+package ua.com.alevel.classes;
+
+public class classB {
+    public void print(String msg) {
+        System.out.println(msg)
+        System.out.println(org.apache.commons.lang3.StringUtils.upperCase(msg));
+    }
+}
